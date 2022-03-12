c1ebec7263f9baf691e433d1f17ba714820bba7d
bennyaharoni <bennyaharoni@gmail.com>
2022-03-12 20:30:12 +0200

    Procfile added

--- envchk.py
+++ envchk.py
@@ -1,6 +1,6 @@
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # install dotenv for python
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
